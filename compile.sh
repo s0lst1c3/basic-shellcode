a612682ef7b4276c03078959602df02ef1f103ef
s0lst1c3 <contact@solstice.me>
2015-03-06 03:30:44 -0500

    looking goood

--- compile.sh
+++ compile.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 nasm -f elf32 -o execve.o execve.asm
 ld -m elf_i386 -o execve execve.o
+shellcode=$(objdump -d execve | grep 8048 | grep -v ">:" | cut -f2 | perl -p -e 's/ [ \n]*/\\x/g')
+
+echo 'Generating shellcode:'
+echo $shellcode
